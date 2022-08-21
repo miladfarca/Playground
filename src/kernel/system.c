82c43d2a7453764f4a3363ad6e36d1471ad24b7f
Milad Fa <miladfarca@gmail.com>
2022-08-21 03:37:27 +0000

    Add comment

--- src/kernel/system.c
+++ src/kernel/system.c
@@ -41,6 +41,7 @@ struct system_time system_get_time() {
   return (struct system_time){s, m, h, am_pm};
 }
 
+// https://wiki.osdev.org/PC_Speaker
 void system_turn_speaker_on(uint32_t nFrequence) {
   uint32_t div;
   uint8_t tmp;
@@ -50,7 +51,6 @@ void system_turn_speaker_on(uint32_t nFrequence) {
   hal_outb(0x42, (uint8_t)(div));
   hal_outb(0x42, (uint8_t)(div >> 8));
 
-  // And play the sound using the PC speaker
   tmp = hal_inb(0x61);
   if (tmp != (tmp | 3)) {
     hal_outb(0x61, tmp | 3);
